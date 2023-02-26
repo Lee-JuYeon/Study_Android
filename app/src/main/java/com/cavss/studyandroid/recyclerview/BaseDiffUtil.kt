29f400fd32f188efaf73923f40ea73a6351cc6b0
cavss <cavss1118@gmail.com>
2023-02-27 02:31:14 +0900

    #022 BaseDifUtil에서 새로 설정할 list의 타입을 mutablelist가 아니라 list로 설정해서 보다 안전하게 파라미터 변경이 가능했고, 무의미하고 반복된 abstract함수 2개를 주석처리함.

--- app/src/main/java/com/cavss/studyandroid/recyclerview/BaseDiffUtil.kt
+++ app/src/main/java/com/cavss/studyandroid/recyclerview/BaseDiffUtil.kt
@@ -3,7 +3,7 @@ package com.cavss.studyandroid.recyclerview
 import androidx.recyclerview.widget.DiffUtil
 
 abstract class BaseDiffUtil<MODEL>(
-    private val oldList : List<MODEL>,
+    private val oldList : MutableList<MODEL>,
     private val newList : List<MODEL>
 ) : DiffUtil.Callback() {
 
@@ -12,14 +12,16 @@ abstract class BaseDiffUtil<MODEL>(
     override fun getNewListSize() = newList.size
 
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return areItemsTheSame(oldList[oldItemPosition], newList[newItemPosition])
+//        return areItemsTheSame(oldList[oldItemPosition], newList[newItemPosition])
+        return oldList[oldItemPosition] == newList[newItemPosition]
     }
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return areContentsTheSame(oldList[oldItemPosition], newList[newItemPosition])
+//        return areContentsTheSame(oldList[oldItemPosition], newList[newItemPosition])
+        return oldList[oldItemPosition] == newList[newItemPosition]
     }
 
-    abstract fun areItemsTheSame(oldItem: MODEL, newItem: MODEL): Boolean
-
-    abstract fun areContentsTheSame(oldItem: MODEL, newItem: MODEL): Boolean
+//    abstract fun areItemsTheSame(oldItem: MODEL, newItem: MODEL): Boolean
+//
+//    abstract fun areContentsTheSame(oldItem: MODEL, newItem: MODEL): Boolean
 }
