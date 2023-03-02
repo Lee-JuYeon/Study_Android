0f98065e47ee9fedfe71dbbaafded8aabf8411ed
cavss <cavss1118@gmail.com>
2023-03-02 12:40:06 +0900

    #048 FrameLayout과 Bottom Navi 연결. Neumorphism 디자인 적용해야함.

--- app/src/main/java/com/cavss/studyandroid/ui/custom/bottomnavigation/FragBottomNavi.kt
+++ app/src/main/java/com/cavss/studyandroid/ui/custom/bottomnavigation/FragBottomNavi.kt
@@ -1,32 +1,52 @@
 package com.cavss.studyandroid.ui.custom.bottomnavigation
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.cavss.studyandroid.R
 import com.cavss.studyandroid.databinding.FragmentBottomnaviBinding
-import com.cavss.studyandroid.ui.screen.menu.MenuVM
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.shape.CornerFamily
-import com.google.android.material.shape.MaterialShapeDrawable
-import com.google.android.material.shape.ShapeAppearanceModel
 
 class FragBottomNavi : Fragment() {
 
-    private val menuVM : MenuVM by activityViewModels()
+    /*
+    ViewMoodelProvider을 사용하지 않아도 자식 fragment들과 VM을 공유가 가능하다.
+    만약 ViewModelProvider 사용하고 싶다면
+     private val vm : VM by lazy {
+        ViewModelProvider(requireActivity()).get(VM::class.java)
+    }
+     */
+    private val bottomNaviVM : BottomNavigationVM by activityViewModels()
     private lateinit var binding : FragmentBottomnaviBinding
+
+    private val fragA = FragA()
+    private val fragB = FragB()
+    private fun changeFragment(page : Int){
+        try{
+            val manager = requireActivity().supportFragmentManager.beginTransaction()
+            when(page){
+                0 -> manager.replace(binding.bottomNaviFrame.id, fragA).commit()
+                1 -> manager.replace(binding.bottomNaviFrame.id, fragB).commit()
+            }
+        }catch (e:Exception){
+            Log.e("mException", "FragBottomNavi, changeFragment  // Exception : ${e.message}")
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         try{
             binding = DataBindingUtil.inflate(inflater, R.layout.fragment_bottomnavi, container, false)
             binding.run{
-                setBottomNavigation(bottomNavigation)
+                setBottomNavigation(bottomNavigation, bottomNaviFrame)
+                setNeumorphismOnBottomNavi(bottomNavigation)
             }
         }catch (e:Exception){
             Log.e("mException", "FragBottomNavi, onCreateView // Exception : ${e.message}")
@@ -34,28 +54,52 @@ class FragBottomNavi : Fragment() {
             return binding.root
         }
     }
+    private fun setNeumorphismOnBottomNavi(bottomNavi: BottomNavigationView){
+        try{
+            bottomNavi.apply {
+                // Bottom Navigation의 배경에 bottomnavi_neumorphic.xml 파일을 적용합니다.
+                setBackgroundResource(R.drawable.bottomnavi_neumorphic_inside)
 
-    private fun setBottomNavigation(bottomNavi : BottomNavigationView){
+                // Bottom Navigation의 아이콘에 Neumorphism 디자인을 적용합니다.
+                itemIconTintList =  ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.purple_200))
+
+                // Bottom Navigation의 텍스트에 Neumorphism 디자인을 적용합니다
+                itemTextColor = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.purple_500))
+
+            }
+        }catch (e:Exception){
+            Log.e("mException", "FragBottomNavi, setNeumorphismOnBottomNavi // Exception : ${e.localizedMessage}")
+        }
+    }
+    private fun setBottomNavigation(bottomNavi : BottomNavigationView, frame : FrameLayout){
         try{
             bottomNavi.setOnItemSelectedListener { menuItem ->
                 when (menuItem.title) {
                     requireContext().getString(R.string.bottom_navi_item1) -> {
-                        // Home Fragment로 이동
+                        // Fragment a로 이동
+                        bottomNaviVM.setFragmentType(0)
                         true
                     }
                     requireContext().getString(R.string.bottom_navi_item2) -> {
-                        // Settings Fragment로 이동
-                        true
-                    }
-                    requireContext().getString(R.string.bottom_navi_item3)-> {
-                        true
-                    }
-                    requireContext().getString(R.string.bottom_navi_item4) -> {
+                        // Fragment b로 이동
+                        bottomNaviVM.setFragmentType(1)
                         true
                     }
                     else -> {
+                        bottomNaviVM.setFragmentType(0)
                         false
                     }
+                }
+            }
+            bottomNaviVM.getFragmentType.observe(requireActivity()){ page : Int ->
+                try {
+                    changeFragment(page)
+                } catch (e: Exception) {
+                    changeFragment(0)
+                    Log.e("mException", "FragBottomNavi, onCreate, bottomNaviVM.getFragmentType.observe // Exception : ${e.message}")
+                } catch (e: NoSuchElementException) {
+                    changeFragment(0)
+                    Log.e("mException","FragBottomNavi, onCreate, bottomNaviVM.getFragmentType.observe  // NoSuchElementException : ${e.message}")
                 }
             }
         }catch (e:Exception){
